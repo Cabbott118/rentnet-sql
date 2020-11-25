b40eac7955b63997f4e99c5f7f12857fb8baf87c
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-11-25 09:47:45 -0500

    Registration route/controller split

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,12 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
-
 const cors = require('cors');
 router.use(cors());
 
 const db = require('../models');
 
-const { validateRegister } = require('../utility/validation');
+const { register } = require('../controllers/userController');
 
 // @route  GET api/users
 // @desc   Display List of Users with their trailers
@@ -52,62 +48,6 @@ router.get('/:uuid', (req, res) => {
 // @route  POST api/users
 // @desc   Register New User
 // @access Public
-router.post('/', (req, res) => {
-  const newUser = {
-    first_name: req.body.first_name.trim(),
-    last_name: req.body.last_name.trim(),
-    email: req.body.email.trim(),
-    password: req.body.password,
-    confirm_password: req.body.confirm_password,
-  };
-
-  const { valid, errors } = validateRegister(newUser);
-  if (!valid) return res.status(400).json(errors);
-  const { first_name, last_name, email, password } = newUser;
-  // Check for existing account by email address
-  db.User.findOne({ where: { email } }).then((user) => {
-    if (user)
-      return res
-        .status(400)
-        .json({ msg: `Account already exists for ${email}` });
-    // Continue if email address is available
-    db.User.create({
-      first_name,
-      last_name,
-      email,
-      password,
-    }).then((newUserData) => {
-      // Create salt & hash
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUserData.password, salt, (err, hash) => {
-          if (err) throw err;
-          // Hash password
-          newUserData.password = hash;
-          // After hash is complete, save newUserData object
-          newUserData.save().then((user) => {
-            // Pass in token and assign it to user id
-            jwt.sign(
-              { id: user.id },
-              config.get('jwtSecret'),
-              { expiresIn: 3600 },
-              (err, token) => {
-                if (err) throw err;
-                res.json({
-                  token,
-                  user: {
-                    id: user.id,
-                    first_name: user.first_name,
-                    last_name: user.last_name,
-                    email: user.email,
-                  },
-                });
-              }
-            );
-          });
-        });
-      });
-    });
-  });
-});
+router.post('/', register);
 
 module.exports = router;
